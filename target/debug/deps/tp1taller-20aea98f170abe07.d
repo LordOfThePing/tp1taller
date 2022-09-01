bcb8973bba69398399f96cf1fa968b9c8c721759
LordOfThePing <pepe_flynn@hotmail.com>
2022-09-01 17:10:52 -0300

    init

---
+++ target/debug/deps/tp1taller-20aea98f170abe07.d
@@ -0,0 +1,5 @@
+/home/pepeflynn/Documentos/tp1taller/target/debug/deps/tp1taller-20aea98f170abe07: src/main.rs
+
+/home/pepeflynn/Documentos/tp1taller/target/debug/deps/tp1taller-20aea98f170abe07.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/tp1taller.d
@@ -0,0 +1,1 @@
+/home/pepeflynn/Documentos/tp1taller/target/debug/tp1taller: /home/pepeflynn/Documentos/tp1taller/src/main.rs
